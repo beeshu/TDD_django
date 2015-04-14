efb4e8a04d354410f54cb6f57c5d1c528e31753e
beeshu <vicentmj@gmail.com>
2015-04-14 19:43:02 +0800

    rename all item input ids and names. FTs still broken

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -38,3 +38,5 @@ class FunctinalTest(StaticLiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])   
 
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
