eb7483ca383e7907984688d7729c0d0c03882bc7
beeshu <vicentmj@gmail.com>
2015-04-14 16:11:32 +0800

    Enforce model validation in list view

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.utils.html import escape
 from django.template.loader import render_to_string
 
 from lists.views import home_page
@@ -71,7 +72,17 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, '/lists/%d/' % (correct_list.id, ))        
         
-        
+    def test_validation_errors_end_up_on_lists_page(self):
+        list_ = List.objects.create()
+        response = self.client.post(
+            '/lists/%d/' % (list_.id, ),
+            data={'item_text': ''}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'list.html')
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
+    
 class NewListTest(TestCase):
     
     def test_saving_a_POST_request(self):
@@ -95,8 +106,6 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        
-        from django.utils.html import escape
         expected_error = escape("You can't have an empty list item")
         self.assertContains(response, expected_error)
         
