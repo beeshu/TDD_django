a03608bc91d50501222761298ad70e2c1a66ca04
beeshu <vicentmj@gmail.com>
2015-04-25 19:36:00 +0800

    Make the static files site-wide.

--- functional_tests/test_login.py
+++ functional_tests/test_login.py
@@ -31,7 +31,7 @@ class LoginTest(FunctinalTest):
         # Edith goes to the awesome superlists site
         # and notices a "Sign in" link for the first time.
         self.browser.get(self.server_url)
-        self.browser.find_element_by_id('login').click()
+        self.browser.find_element_by_id('id_login').click()
 
         # A persona login box appears
         self.switch_to_new_window('Mozilla Persona')
@@ -40,14 +40,14 @@ class LoginTest(FunctinalTest):
         ## Use mockmyid.com for test email
         self.browser.find_element_by_id(
             'authentication_email'
-        ).send_keys('vicent_shu@mockmyid.com')
+        ).send_keys('beeshu@mockmyid.com')
         self.browser.find_element_by_tag_name('button').click()
 
         # The Persona window closes
         self.switch_to_new_window('To-Do')
 
         # She can see that she is logged in
-        self.wait_for_element_with_id('logout')
+        self.wait_for_element_with_id('id_logout')
         navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn('vicent_shu@mockmyid.com', navbar.text)
+        self.assertIn('beeshu@mockmyid.com', navbar.text)
 
