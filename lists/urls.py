f06c194278edbb6c3f7a18a6266e5789a426cdfc
beeshu <vicentmj@gmail.com>
2015-04-26 14:19:34 +0800

    Test using Ajax to get a json response.

--- lists/urls.py
+++ lists/urls.py
@@ -5,4 +5,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
     url(r'^new$', 'lists.views.new_list', name='new_list'),
+    url(r'^json_home$', 'lists.views.view_json_home', name='view_json_home'),
+    url(r'^json_test$', 'lists.views.json_test', name='json_test'),
 )
--- lists/views.py
+++ lists/views.py
@@ -1,24 +1,28 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
+from django.http import JsonResponse
 
 from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
 
+
 def home_page(request):
     return render(request, 'home.html', {'form': ItemForm()})
+
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ExistingListItemForm(for_list=list_)
-    
+
     if request.method == 'POST':
         form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
             form.save()
             return redirect(list_)
-    
-    return render(request, 'list.html', {'list': list_,'form': form})
-    
+
+    return render(request, 'list.html', {'list': list_, 'form': form})
+
+
 def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
@@ -27,4 +31,11 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
-        
+
+
+def view_json_home(request):
+    return render(request, 'json_test.html')
+
+
+def json_test(request):
+    return JsonResponse({'foo': ['1', '2', '3']})
